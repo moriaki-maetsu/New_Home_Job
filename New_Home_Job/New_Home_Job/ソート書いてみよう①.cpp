204fa46fe5e92c199ce38ce5af74869f753cd48d
moriaki-maetsu <159523732+moriaki-maetsu@users.noreply.github.com>
2024-06-16 17:13:32 +0900

    第7章分

--- New_Home_Job/New_Home_Job/ソート書いてみよう①.cpp
+++ New_Home_Job/New_Home_Job/ソート書いてみよう①.cpp
@@ -1,36 +1,36 @@
-#include<stdio.h>
-#define NUM 5
-int main(void)
-{
-	int test[NUM];
-	int tmp;
-	int i, j, s, t;
-
-	//テストの点数を取得するコード
-	printf("%d人の点数を入力してください。\n", NUM);
-	for (i = 0; i < NUM; i++)
-	{
-		scanf_s("%d", &test[i]);
-	}
-
-	//昇順（テストの点数が高い順）にソート（入れ替え）を行うコード
-	for (s = 0; s < NUM; s++)
-	{
-		for (t = s + 1; t < NUM; t++)
-		{
-			if (test[t] > test[s])
-			{
-				tmp = test[t];
-				test[t] = test[s];
-				test[s] = tmp;
-			}
-		}
-	}
-
-	//入れ替え後の点数を出力するコード
-	for (j = 0; j < NUM; j++)
-	{
-		printf("%d番目の人の点数は%dです。\n", j + 1, test[j]);
-	}
-	return 0;
-}
+//#include<stdio.h>
+//#define NUM 5
+//int main(void)
+//{
+//	int test[NUM];
+//	int tmp;
+//	int i, j, s, t;
+//
+//	//テストの点数を取得するコード
+//	printf("%d人の点数を入力してください。\n", NUM);
+//	for (i = 0; i < NUM; i++)
+//	{
+//		scanf_s("%d", &test[i]);
+//	}
+//
+//	//昇順（テストの点数が高い順）にソート（入れ替え）を行うコード
+//	for (s = 0; s < NUM; s++)
+//	{
+//		for (t = s + 1; t < NUM; t++)
+//		{
+//			if (test[t] > test[s])
+//			{
+//				tmp = test[t];
+//				test[t] = test[s];
+//				test[s] = tmp;
+//			}
+//		}
+//	}
+//
+//	//入れ替え後の点数を出力するコード
+//	for (j = 0; j < NUM; j++)
+//	{
+//		printf("%d番目の人の点数は%dです。\n", j + 1, test[j]);
+//	}
+//	return 0;
+//}
---
+++ New_Home_Job/New_Home_Job/文字列配列書いてみよう①.cpp
@@ -0,0 +1,15 @@
+//#include<stdio.h>
+//int main(void)
+//{
+//	char moji[] = "Hello Word";
+//	char str[100];
+//
+//	printf("%s\n", moji);
+//
+//	printf("•¶š—ñ‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B\n");
+//	scanf_s("%s", str,5);
+//
+//	printf("“ü—Í‚µ‚½•¶š—ñ‚Í%s‚Å‚·B", str);
+//
+//	return 0;
+//}
