b635578027a7a54b9e66aaa92283b76418cab2b5
moriaki-maetsu <159523732+moriaki-maetsu@users.noreply.github.com>
2024-08-26 13:11:36 +0900

    tuika

--- New_Home_Job/New_Home_Job/文字操作関数.cpp
+++ New_Home_Job/New_Home_Job/文字操作関数.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 //#include<stdio.h>
 //#include<ctype.h>
 //int main(void)
@@ -117,3 +118,122 @@
 
 //	return 0;
 //}
+=======
+#include<stdio.h>
+#include<ctype.h>
+int main(void)
+{
+	
+	int a,c;
+	a = 1;
+	int num;
+	printf("•¶Žš‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B\n");
+	c = getchar();
+
+	while (a)
+	{
+
+		printf("\n‚Ç‚Ì•¶Žš•ª—Þ‚ðs‚¢‚Ü‚·‚©H\n");
+		printf("1:isalnum\n");
+		printf("2:isalpha\n");
+		printf("3:isdigit\n");
+		printf("4:islower\n");
+		printf("5:isupper\n");
+		printf("6:isxdigit\n");
+		printf("7:tolower\n");
+		printf("8:toupper\n");
+		printf("0:I—¹\n");
+		scanf_s("%d", &num);
+
+		switch (num)
+		{
+		case 1:
+			//isalnumŠÖ”i•Ï”c‚ªA`Z,a`z,0`9‚Å‚ ‚é‚©‚Ç‚¤‚©j
+			if (isalnum(c))
+			{
+				printf("\n%c‚Í‰p”Žš‚Å‚·B\n", c);
+			}
+			else
+			{
+				printf("\n%c‚Í‰p”Žš‚Å‚Í‚ ‚è‚Ü‚¹‚ñB\n", c);
+			}
+			break;
+		case 2:
+			//isalphaŠÖ”i•Ï”c‚ªA`Z,a`z‚Å‚ ‚é‚©‚Ç‚¤‚©j
+			if (isalpha(c))
+			{
+				printf("\n%c‚Í‰p•¶Žš‚Å‚·B\n", c);
+
+			}
+			else
+			{
+				printf("\n%c‚Í‰p•¶Žš‚Å‚Í‚ ‚è‚Ü‚¹‚ñB\n", c);
+			}
+			break;
+		case 3:
+			//isdigitŠÖ”i•Ï”c‚ª”Žš‚Å‚ ‚é‚©‚Ç‚¤‚©j
+			if (isdigit(c))
+			{
+				printf("\n%c‚Í”Žš‚Å‚·B\n", c);
+			}
+			else
+			{
+				printf("\n%c‚Í”Žš‚Å‚Í‚ ‚è‚Ü‚¹‚ñB\n", c);
+			}
+			break;
+		case 4:
+			//islowerŠÖ”i•Ï”c‚ª¬•¶Žša`‚š‚Å‚ ‚é‚©‚Ç‚¤‚©j
+			if (islower(c))
+			{
+				printf("\n%c‚Í¬•¶Žš‚Å‚·B\n", c);
+			}
+			else
+			{
+				printf("\n%c‚Í¬•¶Žš‚Å‚Í‚ ‚è‚Ü‚¹‚ñB\n", c);
+			}
+			break;
+		case 5:
+			//isupperŠÖ”i•Ï”c‚ª‘å•¶ŽšA`Z‚Å‚ ‚é‚©‚Ç‚¤‚©j
+			if (isupper(c))
+			{
+				printf("\n%c‚Í‘å•¶Žš‚Å‚·B\n", c);
+			}
+			else
+			{
+				printf("\n%c‚Í‘å•¶Žš‚Å‚Í‚ ‚è‚Ü‚¹‚ñB\n", c);
+			}
+			break;
+		case 6:
+			//isxdigitŠÖ”i•Ï”c‚ª16i”0`9CA`FCa`f‚Å‚ ‚é‚©‚Ç‚¤‚©j
+			if (isxdigit(c))
+			{
+				printf("\n%c‚Í‚P‚Ui”‚Å‚·B\n", c);
+			}
+			else
+			{
+				printf("\n%c‚Í‚P‚Ui”‚Å‚Í‚ ‚è‚Ü‚¹‚ñB\n", c);
+			}
+			break;
+		case 7:
+			//tolowerŠÖ”i•Ï”c‚ð¬•¶Žša`‚š‚É‚·‚éj
+
+			printf("\n%c‚Í¬•¶Žš‚Å‚·B\n",tolower(c));
+
+			break;
+		case 8:
+			//toupperŠÖ”i•Ï”c‚ð‘å•¶ŽšA`Z‚É‚·‚éj
+			
+			printf("\n%c‚Í‘å•¶Žš‚Å‚·B\n", toupper(c));
+									
+			break;
+
+		default:
+			a = 0;
+			break;
+		}
+
+
+
+	}
+}
+>>>>>>> Stashed changes
