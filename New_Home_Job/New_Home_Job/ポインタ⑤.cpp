ebe1aa40c3b62359126107d4e8ce41120ab668d9
moriaki-maetsu <159523732+moriaki-maetsu@users.noreply.github.com>
2024-10-15 22:43:23 +0900

    10/15Home

--- New_Home_Job/New_Home_Job/ポインタ⑤.cpp
+++ New_Home_Job/New_Home_Job/ポインタ⑤.cpp
@@ -1,16 +1,16 @@
-#include<stdio.h>
-
-int main(void)
-{
-	int ary[5] = { 81,20,67,13,95 };
-	
-
-	printf("%d\n", ary[0]);
-	printf("%d\n", *ary);
-	printf("%p\n", &ary[0]);
-	printf("%p\n", ary);
-	printf("%p\n", ary+1);
-	printf("%d\n", *ary+1);
-	printf("%d\n", *(ary+1));
-	
-}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	int ary[5] = { 81,20,67,13,95 };
+//	
+//
+//	printf("%d\n", ary[0]);
+//	printf("%d\n", *ary);
+//	printf("%p\n", &ary[0]);
+//	printf("%p\n", ary);
+//	printf("%p\n", ary+1);
+//	printf("%d\n", *ary+1);
+//	printf("%d\n", *(ary+1));
+//	
+//}
---
+++ New_Home_Job/New_Home_Job/ポインタ⑥.cpp
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	const char* str = "Hello";
+//	char ary[] = "Hello";
+//
+//	printf("•¶š—ñ‚Í%s‚Å‚·B\n", str);
+//	printf("•¶š—ñ‚Í%s‚Å‚·B\n", ary);
+//
+//
+//	return 0;
+//}
