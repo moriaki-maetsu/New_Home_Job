506cd12e50b2e337089b050431d297a907cd74eb
moriaki-maetsu <159523732+moriaki-maetsu@users.noreply.github.com>
2024-10-16 16:20:46 +0900

    10/16

--- New_Home_Job/New_Home_Job/ポインタ.練習問題①.cpp
+++ New_Home_Job/New_Home_Job/ポインタ.練習問題①.cpp
@@ -1,14 +1,14 @@
 //#include<stdio.h>
 //
-//int Add(int *num1,int*num2)
+//int Add(int* num1,int* num2)
 //{
-//	int add;
+//	int point;
 //	printf("加算する点数を入力してください。\n");
-//	scanf_s("%d", &add);
-//	*num1 = *num1 + add;
-//	*num2 = *num2 + add;
+//	scanf_s("%d", &point);
+//	*num1 = *num1 + point;
+//	*num2 = *num2 + point;
 //
-//	return add;
+//	return point;
 //}
 //
 //int main(void)
@@ -18,7 +18,9 @@
 //	printf("２科目の点数を入力。\n");
 //	scanf_s("%d", &num1);
 //	scanf_s("%d", &num2);
+//
 //	add = Add(&num1, &num2);
+//
 //	printf("%d点加算しましたので\n",add );
 //	printf("科目1は%d点\n",num1 );
 //	printf("科目2は%d点\n",num2 );
--- New_Home_Job/New_Home_Job/ポインタ⑤.cpp
+++ New_Home_Job/New_Home_Job/ポインタ⑤.cpp
@@ -1,16 +1,35 @@
-//#include<stdio.h>
-//
-//int main(void)
-//{
-//	int ary[5] = { 81,20,67,13,95 };
-//	
-//
-//	printf("%d\n", ary[0]);
-//	printf("%d\n", *ary);
-//	printf("%p\n", &ary[0]);
-//	printf("%p\n", ary);
-//	printf("%p\n", ary+1);
-//	printf("%d\n", *ary+1);
-//	printf("%d\n", *(ary+1));
-//	
-//}
+#include<stdio.h>
+
+int main(void)
+{
+	int ary[5] = { 81,20,67,13,95 };
+	int* p;
+
+	p = ary;
+	
+	printf("%d\n", ary[0]);
+	printf("%d\n", *ary);
+	printf("%d\n", *p);
+
+printf("%d\n", *p);
+	printf("%d\n", ary[0]+1);
+	printf("%d\n", *ary+1);
+	
+	printf("%d\n", *(ary+1));
+	printf("%p\n", ary);
+	
+
+
+
+
+
+
+
+
+	/*printf("%p\n", &ary[0]);
+	printf("%p\n", ary);*/
+	/*printf("%p\n", ary+1);
+	printf("%d\n", *ary+1);
+	printf("%d\n", *(ary+1));*/
+	
+}
